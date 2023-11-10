a661ea5cc7dfe6196e9d6768869406f0c80446d0
Alisson Youssf <youssfbr@gmail.com>
2023-11-10 19:07:02 -0300

    Log message

---
+++ src/application/Main.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.logging.Logger;
+
+/**
+ * @author "Alisson Youssf"
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logger logger = Logger.getLogger(Main.class.getName());
+        logger.info("Hello Felipão!");
+
+    }
+}
